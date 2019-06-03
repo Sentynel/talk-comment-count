d19266c28c9695b0d99341bea7454c8d6dfd15db
Sam Lade <sam@sentynel.com>
2019-06-03 23:39:23 +0100

    handle mongo exceptions

--- talk-comment-count.php
+++ talk-comment-count.php
@@ -5,18 +5,22 @@
  * Author: Sam Lade
  */
 function amg_talk_comment_count ($count, $post_id) {
-    $mng = new MongoDB\Driver\Manager("mongodb://localhost");
-    $url = get_permalink($post_id);
-    $assetq = new MongoDB\Driver\Query(["url"=>$url], ["limit" => 1, "projection" => ["id" => ""]]);
-    $assets = $mng->executeQuery("talk.assets", $assetq)->toArray();
-    // error handle
-    if (count($assets) == 0) {
-        return 0;
+    try {
+        $mng = new MongoDB\Driver\Manager("mongodb://localhost");
+        $url = get_permalink($post_id);
+        $assetq = new MongoDB\Driver\Query(["url"=>$url], ["limit" => 1, "projection" => ["id" => ""]]);
+        $assets = $mng->executeQuery("talk.assets", $assetq)->toArray();
+        // error handle
+        if (count($assets) == 0) {
+            return 0;
+        }
+        $assetid = $assets[0]->id;
+        $countq = new MongoDB\Driver\Command(['count'=>'comments', 'query'=>['asset_id'=>$assetid, 'status'=>['$ne'=>'REJECTED']]]);
+        $countres = $mng->executeCommand('talk', $countq)->toArray();
+        return $countres[0]->n;
+    } catch (exception $e) {
+        return $count;
     }
-    $assetid = $assets[0]->id;
-    $countq = new MongoDB\Driver\Command(['count'=>'comments', 'query'=>['asset_id'=>$assetid, 'status'=>['$ne'=>'REJECTED']]]);
-    $count = $mng->executeCommand('talk', $countq)->toArray();
-    return $count[0]->n;
 }
 
 add_filter('get_comments_number', 'amg_talk_comment_count', 10, 2);
