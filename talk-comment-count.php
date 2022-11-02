c22149a647ccaf750a957a979e09b9443eb7b34e
Sam Lade <sam@sentynel.com>
2022-11-02 15:02:51 +0000

    caching for comment counts

--- talk-comment-count.php
+++ talk-comment-count.php
@@ -5,8 +5,18 @@
  * Author: Sam Lade
  */
 function amg_talk_comment_count ($count, $post_id) {
+    static $mng = false;
+
+    $key = "comment_count_{$post_id}";
+    $res = wp_cache_get($key, '');
+    if ($res !== false) {
+        return $res;
+    }
     try {
-        $mng = new MongoDB\Driver\Manager("mongodb://localhost");
+        if ($mng === false) {
+            // cache Mongo connection
+            $mng = new MongoDB\Driver\Manager("mongodb://localhost");
+        }
         $url = get_permalink($post_id);
         $url = preg_replace_callback(
             "/%[a-f0-9]{2}/",
@@ -18,6 +28,7 @@ function amg_talk_comment_count ($count, $post_id) {
         $assetq = new MongoDB\Driver\Query(["url"=>$url], ["limit" => 1, "projection" => ["commentCounts.status" => ""]]);
         $assets = $mng->executeQuery("coral.stories", $assetq)->toArray();
         $newcount = $assets[0]->commentCounts->status->APPROVED + $assets[0]->commentCounts->status->NONE;
+        wp_cache_set($key, $newcount, '', 300);
         return $newcount;
     } catch (exception $e) {
         return $count;
